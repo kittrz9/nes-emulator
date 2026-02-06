129fbbd03b2e1ef6b0874f93aa23f8a859c70151
kittrz <kittrz@kittrz.gay>
2026-02-06 13:14:53 -0600

    started to implement nes 2.0 header stuff

--- src/files.c
+++ src/files.c
@@ -9,6 +9,36 @@
 #include "ppu.h"
 #include "rom.h"
 
+// https://www.nesdev.org/wiki/INES
+typedef struct {
+	uint8_t NES[4];
+	uint8_t prgSize; // 16kb clusters
+	uint8_t chrSize; // 8kb clusters
+	uint8_t flags6;
+	uint8_t flags7;
+	uint8_t flags8;
+	uint8_t flags9;
+	uint8_t flags10;
+	uint8_t padding[5];
+} iNESHeader;
+
+// https://www.nesdev.org/wiki/NES_2.0
+typedef struct {
+	uint8_t NES[4];
+	uint8_t prgSizeLSB;
+	uint8_t chrSizeLSB;
+	uint8_t flags6;
+	uint8_t flags7;
+	uint8_t mapperMSB;
+	uint8_t romSizeMSB;
+	uint8_t prgRAMSize;
+	uint8_t chrRAMSize;
+	uint8_t timing;
+	uint8_t flags13;
+	uint8_t miscROMs;
+	uint8_t expansionDevice;
+} nes2Header;
+
 uint8_t loadROM(const char* path) {
 	FILE* f = fopen(path, "rb");
 	if(!f) {
@@ -27,51 +57,93 @@ uint8_t loadROM(const char* path) {
 
 	fclose(f);
 
-	iNESHeader* header = (iNESHeader*)fileBuffer;
-
-	if(strncmp((char*)header->NES, "NES\x1A", 4) != 0) {
+	if(strncmp((char*)fileBuffer, "NES\x1A", 4) != 0) {
 		printf("invalid header\n");
 		return 1;
 	}
 
-	if((header->flags7 & 0x0C) == 0x08) {
-		printf("NES 2.0 rom\n");
-
-	}
-
-	if(header->flags6 & 0x02) {
+	// common between formats
+	ppu.mirror = fileBuffer[6] & 0x1;
+	if(fileBuffer[6] & 0x02) {
 		printf("battery backed PRG RAM, unsupported\n");
 	}
 
-	printf("PRG ROM size: %uk\n", header->prgSize*16);
-	printf("CHR ROM size: %uk\n", header->chrSize*8);
-	uint8_t mapperID = ((header->flags6 & 0xF0) >> 4) | ((header->flags7 & 0xF0));
+	uint8_t* prgLocation;
+	uint8_t* chrLocation;
+	prgSize = 0;
+	chrSize = 0;
+	size_t chrRAMSize = 0;
+	uint16_t mapperID;
+
+	if((fileBuffer[7] & 0x0C) == 0x08) {
+		printf("NES 2.0 rom\n");
+		nes2Header* header = (nes2Header*)fileBuffer;
+
+		printf("timing: %02X\n", header->timing & 0x3);
+
+		if((header->romSizeMSB & 0xF) == 0xF) {
+			// exponent notation
+			uint8_t mult = (header->prgSizeLSB & 0x3)*2 + 1;
+			uint8_t exponent = header->prgSizeLSB >> 2;
+			prgSize = (1<<exponent) * mult;
+		} else {
+			prgSize = (((header->romSizeMSB & 0xF) << 8) | header->prgSizeLSB) * 0x4000;  
+		}
+
+		if((header->romSizeMSB & 0xF0) == 0xF0) {
+			// exponent notation
+			uint8_t mult = (header->chrSizeLSB & 0x3)*2 + 1;
+			uint8_t exponent = header->chrSizeLSB >> 2;
+			chrSize = (1<<exponent) * mult;
+		} else {
+			// already shifted by 4
+			chrSize = (((header->romSizeMSB & 0xF0) << 4) | header->chrSizeLSB) * 0x2000;  
+		}
+
+		if((header->chrRAMSize & 0xF) != 0) {
+			chrRAMSize = 64 << (header->chrRAMSize & 0xF);
+		}
+
+
+		// still need to implement the msb for mapper ids and submapper ids
+		mapperID = ((header->flags6 & 0xF0) >> 4) | ((header->flags7 & 0xF0));
+	} else {
+		iNESHeader* header = (iNESHeader*)fileBuffer;
+
+		mapperID = ((header->flags6 & 0xF0) >> 4) | ((header->flags7 & 0xF0));
+
+		prgSize = header->prgSize * 0x4000;
+		chrSize = header->chrSize * 0x2000;
+
+		if(chrSize == 0) {
+			chrRAMSize = 0x2000;
+		}
+	}
+	printf("PRG ROM size: %luk\n", prgSize / 0x400);
+	printf("CHR ROM size: %luk\n", chrSize / 0x400);
+	printf("CHR RAM size: %luk\n", chrRAMSize / 0x400);
+	printf("mirror: %02X\n", ppu.mirror);
 	printf("mapper ID: %02X\n", mapperID);
 
 	setMapper(mapperID);
 
-	uint8_t* prgLocation = fileBuffer+sizeof(iNESHeader);
-	if(header->flags6 & 0x04) {
+	prgLocation = fileBuffer+16;
+	if(fileBuffer[6] & 0x04) {
 		printf("trainer in rom\n");
 		prgLocation += 512;
 	}
+	chrLocation = prgLocation + prgSize;
 
-	prgSize = header->prgSize * 0x4000;
-	chrSize = header->chrSize * 0x2000;
-	uint8_t* chrLocation = prgLocation + prgSize;
-
-	prgROM = malloc(prgSize);
-
-	memcpy(prgROM, prgLocation, prgSize);
+	if(prgSize != 0) {
+		prgROM = malloc(prgSize);
+		memcpy(prgROM, prgLocation, prgSize);
+	}
 	if(chrSize != 0) {
 		chrROM = malloc(chrSize);
 		memcpy(chrROM, chrLocation, chrSize);
-	} else {
-		chrROM = malloc(0x2000);
+	} else if(chrRAMSize != 0) {
+		chrROM = malloc(chrRAMSize); // there's probably some things that bank switch between chr rom and chr ram, this needs to be fixed
 	}
-
-	ppu.mirror = header->flags6 & 0x1;
-	printf("mirror: %02X", ppu.mirror);
 
 	free(fileBuffer);
 
